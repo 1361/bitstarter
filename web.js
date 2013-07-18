313c2e298b29fe817a0045305af12fc3ee6ded38
andrew.minarick <andrew.minarick>
2013-07-18 18:58:14 +0000

    updated web.js with index.html

--- web.js
+++ web.js
@@ -1,10 +1,14 @@
 var express = require('express');
-
+var fs = require('fs');  
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('index.html');
-});
+  var buffer = new Buffer();
+  response.send(fs.readFileSync('index.html'), function (err,data) {
+  if (err) throw err;
+  console.log(data);
+  });
+  });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
